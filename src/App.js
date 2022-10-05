4b1ce831f37a53a3149d0531923c74b4b56a2904
jaakko0354 <koobixp1@gmail.com>
2022-10-05 11:37:24 +0300

    splitted todolist and app. todolist component is just the list handling. <buttons and functions are in app.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import'./App.css';
-import ToDoList from'./ToDoList';
+import React from 'react';
+import './App.css';
+import Todotable from './components/ToDoList';
+function App(){    
+    const[desc, setDesc] = React.useState({description: "", day: "" });
+    const[todos, setTodos] = React.useState([]);
 
-function App() {
+  const addToDo = (event) => {
+    setTodos([desc, ...todos]);
+    setDesc({description: "", day: "" })
+  };
+  const syoteKayttaja = (event) => {
+    setDesc({...desc, [event.target.name]: event.target.value});
+  };
+  function deleteButton(index){
+    const filtered = todos.filter((desc, i) => i !== index);
+    
+    setTodos(filtered);
+  }
+  
   return (
-    <div className='App'>
-      <ToDoList />
+    <div className="App">
+      <h1>Add to do:</h1>
+        <input placeholder="Description" type="description" name="description" value={desc.description} onChange={syoteKayttaja}/>
+        <input placeholder="Date:" type="day" value={desc.day} name="day" onChange={syoteKayttaja}/>
+        <button onClick={addToDo}>Add</button>  
+        <Todotable todos={todos} deleteButton={deleteButton}/>
     </div>
   );
-}
-
+};
 export default App;
