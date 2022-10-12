063c891ab9389c470cf08bef58439b9887ee3adb
jaakko0354 <koobixp1@gmail.com>
2022-10-12 12:51:15 +0300

    added more ag grid components

--- src/App.js
+++ src/App.js
@@ -1,30 +1,66 @@
 import React from 'react';
 import './App.css';
 import Todotable from './components/ToDoList';
-function App(){    
-    const[desc, setDesc] = React.useState({description: "", day: "" });
-    const[todos, setTodos] = React.useState([]);
+import { AgGridReact } from 'ag-grid-react'; // the AG Grid React Component
+import {Stack} from '@mui/system';
+import { Button } from '@mui/material';
 
+import 'ag-grid-community/styles/ag-grid.css'; // Core grid CSS, always needed
+import 'ag-grid-community/styles/ag-theme-material.css'; // Optional theme CSS
+import { grid } from '@mui/system';
+function App(){    
+    const[desc, setDesc] = React.useState({description: "", day: "", priority:"" });
+    const[todos, setTodos] = React.useState([]);
+    const gridRef = React.useRef(); 
+    const columns = [
+    {field:"description", sortable:true, filter:true, floatingFilter:true},
+    {field:"day",sortable:true, filter:true, floatingFilter:true},
+    {field:"priority",sortable:true, filter:true, floatingFilter:true,  cellStyle: params => params.value === "High" ? {color:'red'}:{color:'black'}}
+  ]
   const addToDo = (event) => {
     setTodos([desc, ...todos]);
-    setDesc({description: "", day: "" })
+    setDesc({description: "", day: "", priority:"" })
   };
   const syoteKayttaja = (event) => {
     setDesc({...desc, [event.target.name]: event.target.value});
   };
-  function deleteButton(index){
-    const filtered = todos.filter((desc, i) => i !== index);
-    
-    setTodos(filtered);
+  const deleteButton=()=>{
+    if(gridRef.current.getSelectedNodes().length > 0){
+      setTodos(todos.filter((desc,index)=>
+      gridRef.current.getSelectedNodes()[0].childIndex !==index))
+    }else{
+      alert('Select row first')
+    }
+
   }
   
   return (
     <div className="App">
       <h1>Add to do:</h1>
-        <input placeholder="Description" type="description" name="description" value={desc.description} onChange={syoteKayttaja}/>
-        <input placeholder="Date:" type="day" value={desc.day} name="day" onChange={syoteKayttaja}/>
-        <button onClick={addToDo}>Add</button>  
+      <Stack spacing={2} direction='row' justifyContent="center" alignItems="center"> 
+      
+        Description: {" "}<input placeholder="Description" type="description" name="description" value={desc.description} onChange={syoteKayttaja}/>
+        Date: {" "}<input placeholder="Date:" type="date" value={desc.day} name="day" onChange={syoteKayttaja}/>
+        Priority: {" "}<input placeholder="Priority:" type="text" value={desc.priority} name="priority" onChange={syoteKayttaja}/>
+               
+          <Button variant='contained' onClick={addToDo}>Add</Button>  
+          <Button variant='contained' color='error' onClick={deleteButton}>Delete</Button>  
+        </Stack>
+
         <Todotable todos={todos} deleteButton={deleteButton}/>
+
+        <div className="ag-theme-material" style ={{height:'800px', width:'40%', margin:'auto'}}>
+          <AgGridReact 
+          columnDefs={columns} 
+          rowData={todos} 
+          rowSelection="single" 
+          ref={gridRef} 
+          animateRows="true"
+          onGridReady={params => gridRef.current = params.api}
+          >
+          </AgGridReact>
+        </div>
+        
     </div>
   );
 };
--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -2,18 +2,7 @@ import React from 'react';
 
 export default function ToDoList(props){
   return(<div>
-      <table>
-          <tbody>
-            <tr><th>Description</th><th>Day</th></tr>
-            {props.todos.map((desc,index) => 
-            <tr key={index}>
-                <td>{desc.description}</td>
-                <td>{desc.day}</td>
-                <td><button onClick={()=>props.deleteButton(index)}>Delete</button></td>
-            </tr>
-            )}
-          </tbody>
-        </table>
+      
     </div>
     );
    
